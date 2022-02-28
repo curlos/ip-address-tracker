48dd38c70e7eb23daa31a930226ced3cb9e7ca1b
Carlos Martinez <curlosmart@gmail.com>
2022-02-28 12:44:33 -0500

    moved assets and styled page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,181 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="topHeader">
+      <div class="headerTitle">IP Address Tracker</div>
+
+      <div class="searchInputWrapper">
+        <input type="text" placeholder="Search for any IP address or domain" v-model="searchText" />
+        <button @click="fetchIpAddress">
+          <img src="/assets/images/icon-arrow.svg" alt="arrow" />
+        </button>
+      </div>
+
+      <div class="ipAddressInfo">
+        <div class="ipAddressCard">
+          <div class="title">IP ADDRESS</div>
+          <div class="info">{{ ipAddress.ip }}</div>
+        </div>
+
+        <div class="ipAddressCard">
+          <div class="title">IP ADDRESS</div>
+          <div class="info">{{ ipAddress.ip }}</div>
+        </div>
+
+        <div class="ipAddressCard">
+          <div class="title">IP ADDRESS</div>
+          <div class="info">{{ ipAddress.ip }}</div>
+        </div>
+
+        <div class="ipAddressCard">
+          <div class="title">IP ADDRESS</div>
+          <div class="info">{{ ipAddress.ip }}</div>
+        </div>
+        
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   mounted() {
-    
-  }
+    axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${process.env.VUE_APP_API_KEY}&ipAddress=8.8.8.8`).then((response) => {
+      console.log(response.data)
+      this.ipAddress = response.data
+    })
+  },
+  data() {
+    return {
+      ipAddress: {},
+      searchText: '',
+      loading: false
+    }
+  },
+  methods: {
+    async fetchIpAddress() {
+      if (this.searchText) {
+        console.log('fetching...')
+        this.loading = true
+      }
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .wrapper {
+    height: 100vh;
+    width: 100vw;
+    max-width: 100%;
+    font-family: Rubik, sans-serif;
+  }
+
+  .topHeader {
+    background-image: url('/assets/images/pattern-bg.png');
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    padding: 15px;
+  }
+
+  .headerTitle {
+    font-weight: medium;
+    color: #FFFFFF;
+    font-size: 20px;
+  }
+
+  .searchInputWrapper {
+    width: 50%;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .searchInputWrapper input {
+    width: 100%;
+    height:100%;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    border: none;
+    padding: 15px 20px;
+    font-size: 18px;
+    font-family: Rubik, sans-serif;
+    box-sizing: border-box;
+  }
+
+  .searchInputWrapper input:focus {
+    outline: none;
+  }
+
+  ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+    color: #969696;
+  }
+  ::-moz-placeholder { /* Firefox 19+ */
+    color: #969696;
+  }
+  :-ms-input-placeholder { /* IE 10+ */
+    color: #969696;
+  }
+  :-moz-placeholder { /* Firefox 18- */
+    color: #969696;
+  }
+
+  .searchInputWrapper button {
+    padding: 16px;
+    background-color: #000000;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    height: 100%;
+    border: none;
+    font-size: 18px;
+  }
+
+  .ipAddressInfo {
+    padding: 20px 5px;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    display: flex;
+
+  }
+
+  .ipAddressCard {
+    font-size: 20px;
+    padding-left: 20px;
+    padding-right: 30px;
+    border-right: 1px solid #D9D9D9;
+  }
+
+  .ipAddressCard:last-of-type {
+    border-right: none;
+  }
+
+  .ipAddressCard .title {
+    color: #969696;
+    font-size: 12px;
+    font-weight: bold;
+    letter-spacing: 1.75px;
+    margin-bottom: 5px;
+  }
+
+  .ipAddressCard .info {
+    font-size: 26px;
+    font-weight: medium;
+    color: #2C2C2C;
+    letter-spacing: -0.23px;
+  }
+
+
 </style>
